0d65123eb3c2ffca03aff9a9e8e478bbcf79c78f
unknown <estudiante@LAB0103.SERVERINT.LOCAL>
2017-03-02 13:33:30 -0600

    iva

---
+++ Elizabeth/script.js
@@ -0,0 +1,9 @@
+var elizabeth = angular.module("elizabeth",[]);
+elizabeth.controller("operaciones",function($scope){
+	$scope.cantidad1 = 12
+	$scope.cantidad2 = 0.12
+
+	$scope.resultado = $scope.cantidad1*$scope.cantidad2
+	})
+
+
